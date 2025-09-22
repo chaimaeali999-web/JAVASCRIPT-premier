ea11600a0dc335a03793e1641542ddb4ef47d6ee
chaymae <chaimaeali999@gmail.com>
2025-09-22 09:52:13 +0100

    master

---
+++ Prototype/script.js
@@ -0,0 +1,16 @@
+function nambrsc() {
+ let nambrs =["c3","c1", "c2", "c1"];
+ let choisi= "c1";
+ let counter = 0;
+for (let i = 0; i < nambrs.length; i++){
+      if (nambrs[i] === choisi) {
+          counter++;
+       
+    }
+    console.log("all is here "+counter);
+}
+
+// ent of script.js
+
+}
+nambrsc();
